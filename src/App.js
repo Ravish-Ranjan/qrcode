d7eab6a5bec21757ac6f0406986be1f38c503d00
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-12-19 16:27:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { Typography, Button, TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import QrCode from "react-qr-code";
 import Download from "./Download";
 import html2canvas from "html2canvas";
@@ -7,81 +7,86 @@ import download from "downloadjs";
 import "./app.css";
 
 function App() {
-	const [value, setValue] = useState("");
-	const qrcodeRef = useRef(null);
-	const [config, setCol] = useState({
-		fgColor: "#000000",
-		bgColor: "#ffffff",
-	});
+    const [value, setValue] = useState("");
+    const qrcodeRef = useRef(null);
+    const [config, setCol] = useState({
+        fgColor: "#000000",
+        bgColor: "#ffffff",
+    });
 
-	const handleDownload = () => {
-		html2canvas(qrcodeRef.current).then((canvas) => {
-			const dataUrl = canvas.toDataURL();
-			download(dataUrl, `qrcode_${value.replace(" ", "_")}.png`);
-		});
-	};
+    const handleDownload = () => {
+        html2canvas(qrcodeRef.current).then((canvas) => {
+            const dataUrl = canvas.toDataURL();
+            download(dataUrl, `qrcode_${value.trim().replace(" ", "_")}.png`);
+        });
+    };
 
-	return (
-		<div className="card">
-			<Typography variant="h4" className="heading">
-				Qr-Code Generator
-			</Typography>
-			<div
-				style={{
-					width: "100%",
-					height: "auto",
-					overflowX: "auto",
-					display: "grid",
-					placeItems: "center",
-					outline: "1px solid black",
-					borderRadius: ".5rem",
-					padding:"1rem"
-				}}
-				ref={qrcodeRef}
-			>
-				<QrCode
-					value={value === "" ? "qrcode" : value}
-					{...config}
-					className="code"
-				/>
-			</div>
-			<TextField
-				label="Enter QrCode Value"
-				value={value}
-				onChange={(e) => {
-					setValue(e.target.value);
-				}}
-			/>
-			<div className="inline">
-				<TextField
-					value={config.fgColor}
-					type="color"
-					label="Qr Color"
-					className="color"
-					onChange={(e) => {
-						setCol({ ...config, fgColor: e.target.value });
-					}}
-				/>
-				<TextField
-					value={config.bgColor}
-					type="color"
-					label="Background Color"
-					className="color"
-					onChange={(e) => {
-						setCol({ ...config, bgColor: e.target.value });
-					}}
-				/>
-			</div>
-			<Button
-				disabled={value === ""}
-				variant="contained"
-				endIcon={<Download />}
-				onClick={handleDownload}
-			>
-				Download
-			</Button>
-		</div>
-	);
+    return (
+        <div
+            className="h-5/6 p-4 rounded-lg shadow-md flex flex-col items-center justify-center space-y-4 overflow-auto"
+            style={{
+                width: "400px",
+                maxWidth: "90vw",
+                backgroundColor: "#f3f4f6cc",
+            }}
+        >
+            <span className="text-3xl font-semibold text-start w-full">
+                Qr-Code Generator
+            </span>
+            <div
+                ref={qrcodeRef}
+                className="p-4 grid place-items-center aspect-square min-w-4/5 rounded-lg"
+                style={{ backgroundColor: config.bgColor }}
+            >
+                <QrCode
+                    value={
+                        value.trim() === ""
+                            ? "https://ravish-ranjan.github.io/qrcode"
+                            : value.trim()
+                    }
+                    {...config}
+                    className="code"
+                />
+            </div>
+            <TextField
+                label="Enter QrCode Value"
+                value={value}
+                className="w-full"
+                onChange={(e) => {
+                    setValue(e.target.value);
+                }}
+            />
+            <div className="flex justify-between w-full items-stretch">
+                <TextField
+                    value={config.fgColor}
+                    type="color"
+                    label="Qr Color"
+                    className="w-1/2"
+                    onChange={(e) => {
+                        setCol({ ...config, fgColor: e.target.value });
+                    }}
+                />
+                <TextField
+                    value={config.bgColor}
+                    type="color"
+                    label="Background Color"
+                    className="w-1/2"
+                    onChange={(e) => {
+                        setCol({ ...config, bgColor: e.target.value });
+                    }}
+                />
+            </div>
+            <Button
+                disabled={value.trim() === ""}
+                variant="contained"
+                endIcon={<Download />}
+                onClick={handleDownload}
+                className="w-full"
+            >
+                Download
+            </Button>
+        </div>
+    );
 }
 
 export default App;
